f7b1b7548b14acc773f90928723a31a7633d8ddf
Camille <83260908+Camille846@users.noreply.github.com>
2022-06-25 21:53:22 -0300

    Feat: Create handleSubmit Function to send form data to Google Sheets

---
+++ main.js
@@ -0,0 +1,19 @@
+const handleSubmit = (event)=>{
+    event.preventDefault();
+
+    const name = document.querySelector("input[name=name]").value;
+    const email = document.querySelector("input[name=email]").value;
+
+    fetch('https://api.sheetmonkey.io/form/gGtbNc86zkSBYEsXnyhsmc', {
+
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type':'application/json',
+        },
+
+        body: JSON.stringify({name, email}),
+    });
+}
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
