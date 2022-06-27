e41b824b9923be8b9e368a1cb8b98ceee5f7f3dd
Camille <83260908+Camille846@users.noreply.github.com>
2022-06-27 16:30:41 -0300

    Feat: active loading animation after Click Event

--- main.js
+++ main.js
@@ -17,3 +17,18 @@ const handleSubmit = (event)=>{
 }
 
 document.querySelector("form").addEventListener("submit", handleSubmit);
+
+document.addEventListener('DOMContentLoaded', function () {
+    const btn = document.querySelector('.submit'),
+        loader = document.querySelector('.loader'),
+        check = document.querySelector('.check');
+    
+    btn.addEventListener('click', function () {
+      loader.classList.add('active');    
+    });
+   
+    loader.addEventListener('animationend', function() {
+      check.classList.add('active'); 
+    });
+});
+  
